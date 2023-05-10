e8e34c2a7d086ca3f7cc3a43e3c5c159448c99d8
eiscan <eiscan@outlook.com.tr>
2023-05-10 14:34:44 +0300

    added entities

---
+++ ProductManagement.Core/ProductFeature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Core
+{
+    public class ProductFeature
+    {
+        public Guid Id { get; set; }
+        public string Color { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
