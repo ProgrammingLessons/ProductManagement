afbabfcdd9f15001e76d0e0f4724da0ba6945125
eiscan <eiscan@outlook.com.tr>
2023-05-24 13:31:01 +0300

    ADD GenericRepository

---
+++ ProductManagement.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ProductManagement.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void RemoveRange(IEnumerable<T> entities);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
+    }
+}
